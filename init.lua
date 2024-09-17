576dba773e90a7d143621ac89d0c1cf7a80d9384
Franco Neo Recasata <neorecasata@gmail.com>
2024-09-17 17:06:02 +0800

    Add dart to treesitter languages

--- init.lua
+++ init.lua
@@ -1193,7 +1193,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'yaml', 'python' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'yaml', 'python', 'dart' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
