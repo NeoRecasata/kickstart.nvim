12369c3578a6b4dfe637214a5fd38ff4a50cf43e
Neo R. <neorecasata@gmail.com>
2026-03-19 22:14:35 -0700

    feat: add nvim-tree, dashboard, flutter-tools, oxocarbon theme
    - Swap tokyonight for oxocarbon colorscheme
    - Add nvim-tree file explorer (<leader>e)
    - Add dashboard-nvim with custom NEO ASCII header
    - Add flutter-tools.nvim (<leader>F)
    - Disable netrw, enable Nerd Font
    - Enable custom plugins import

--- init.lua
+++ init.lua
@@ -84,6 +84,10 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -91,7 +95,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -614,7 +618,7 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'lua_ls', -- Lua Language server
+        'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
       })
@@ -787,26 +791,12 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'nyoom-engineering/oxocarbon.nvim',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.opt.background = 'dark' -- or "light"
+      vim.cmd.colorscheme 'oxocarbon'
     end,
   },
 
@@ -881,7 +871,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,58 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    cmd = { 'NvimTreeToggle', 'NvimTreeOpen', 'NvimTreeFocus' },
+    keys = {
+      { '<leader>e', '<cmd>NvimTreeToggle<CR>', desc = 'File [E]xplorer' },
+    },
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function() require('nvim-tree').setup {} end,
+  },
+  {
+    'nvimdev/dashboard-nvim',
+    lazy = false,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      theme = 'doom',
+      config = {
+        header = {
+          '',
+          ' ███╗   ██╗███████╗ ██████╗ ',
+          ' ████╗  ██║██╔════╝██╔═══██╗',
+          ' ██╔██╗ ██║█████╗  ██║   ██║',
+          ' ██║╚██╗██║██╔══╝  ██║   ██║',
+          ' ██║ ╚████║███████╗╚██████╔╝',
+          ' ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ',
+          '',
+          '',
+        },
+        center = {
+          { icon = '  ', desc = 'Find File        ', key = 'f', action = 'Telescope find_files' },
+          { icon = '  ', desc = 'Recent Files     ', key = 'r', action = 'Telescope oldfiles' },
+          { icon = '  ', desc = 'Find Word        ', key = 'g', action = 'Telescope live_grep' },
+          { icon = '  ', desc = 'File Tree        ', key = 'e', action = 'NvimTreeToggle' },
+          { icon = '  ', desc = 'Quit             ', key = 'q', action = 'qa' },
+        },
+      },
+    },
+  },
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+      require('flutter-tools').setup {
+        vim.keymap.set('n', '<leader>F', ':Telescope flutter commands<CR>', { desc = '[F]lutter Commands', silent = true }),
+      }
+    end,
+  },
+}
